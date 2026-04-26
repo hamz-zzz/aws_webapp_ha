d04c4b86c851b66e50e9f71f68ae6a8ae465a977
Hamza Mahmood <hamzamahmood2k@gmail.com>
2026-04-26 16:24:33 -0400

    some stuff

--- configs/user-data.sh
+++ configs/user-data.sh
@@ -4,8 +4,7 @@ dnf install -y httpd
 systemctl start httpd
 systemctl enable httpd
 
-TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
--H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
 INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
 
